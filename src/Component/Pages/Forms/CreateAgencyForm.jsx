c334cf774ca23fd696c63a543c4df5b0a3587b6d
Janvi Jawhrani <janvijawhrani@gmail.com>
2025-09-19 14:19:14 +0530

    vendor agencey conectivity done by janvi

--- src/Component/Pages/Forms/CreateAgencyForm.jsx
+++ src/Component/Pages/Forms/CreateAgencyForm.jsx
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { X } from "lucide-react";
 
 export default function CreateAgencyForm({ setAgencyForm }) {
+
+  const [VValue, setInputValue] = useState({
+    Name: "",
+    type: ""
+  });
+  const handleChange = (e) => {
+    setInputValue({
+      ...VValue, [e.target.name]: e.target.value
+    })
+  }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    //send request to backend
+    const res = await fetch("http://localhost:5001/api/venderagencey", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(VValue),
+
+    });
+    if (res.ok) {
+      alert("Request Sent Successfully");
+      setInputValue({
+        Name: "",
+        type: ""
+      });
+    } else {
+      alert("Sending Error");
+    }
+
+  };
+
+
+
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-40">
       <div className="relative w-full max-w-md bg-white p-6 rounded-2xl shadow-lg">
-        
+
         {/* Close Button */}
         <button
           type="button"
@@ -17,25 +50,29 @@ export default function CreateAgencyForm({ setAgencyForm }) {
 
         <h2 className="text-xl font-bold mb-4 text-gray-800">Create Agency</h2>
 
-        <form className="space-y-3">
- 
+        <form onSubmit={handleSubmit} className="space-y-3">
 
-         Vendor Ttype <input
+
+          Vendor Name <input
             type="text"
-            name="V_name"
-            placeholder="Vendor Type"
+            name="Name"
+            value={VValue.Name}
+            onChange={handleChange}
+            id="name"
+            className="w-full p-2 border rounded"
+            required
+          />
+          Vendor Type<input
+            type="text"
+            id="type"
+            name="type"
+            value={VValue.type}
+            onChange={handleChange}
             className="w-full p-2 border rounded"
             required
           />
 
 
-         Vendor Name <input
-            type="text"
-            name="V_name"
-            placeholder="Vendor Name"
-            className="w-full p-2 border rounded"
-            required
-          />
 
 
           <button
